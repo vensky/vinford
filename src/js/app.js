524111e79b91912ccc0c06b3ba589de91e22338d
vensky <ivan.fedotovskikh@gmail.com>
2021-09-10 15:20:41 +0500

    Добавляет верстку Шапки и раздела О нас

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,5 @@
 'use strict';
 
-@@include('./_components/_swiper.js');
+//@@ includ('./_components/_swiper.js');
 
-const swiperProjects = new Swiper('.projects__swiper', {
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination--projects',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next--projects',
-        prevEl: '.swiper-button-prev--projects',
-    },
-});
 
-const swiperPersonal = new Swiper('.personal__swiper', {
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination--personal',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next--personal',
-        prevEl: '.swiper-button-prev--personal',
-    },
-});
-
-const swiperBonus = new Swiper('.bonus__swiper', {
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination--bonus',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next--bonus',
-        prevEl: '.swiper-button-prev--bonus',
-    },
-});
