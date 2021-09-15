67fa468dbbd398325833696a6920bb402fe6aa86
vensky <ivan.fedotovskikh@gmail.com>
2021-09-15 14:51:57 +0500

    Добавляет анимацию раздела Преимуществ

---
+++ src/js/_components/_tabs.js
@@ -0,0 +1,19 @@
+const clickToTab = () => {
+    const $tabs = document.querySelector('.tabs');
+    const $feature = document.querySelector('.features__content');
+    const $featureTitle = $feature.querySelector('.features__subtitle');
+    const $featureDesc = $feature.querySelector('.features__desc');
+    const $featureImg = $feature.querySelector('.featute__img-source');
+
+    $tabs.addEventListener('click', (event) => {
+        let tab = event.target.closest('.tab');
+        [...$tabs.querySelectorAll('.tab')].forEach((tab) => tab.classList.remove('tab--active'));
+        tab.classList.add('.tab--active');
+        $featureTitle.textContent = tab.textContent;
+        $featureDesc.textContent = tab.dataset.featureDesc;
+        $featureImg.setAttribute('src', `img\/${tab.dataset.featureImg}`);
+        $featureImg.setAttribute('alt', $featureDesc.textContent)
+    })
+}
+
+clickToTab();
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
 'use strict';
 
+@@include('./_vendor/_swiper-bundle.js');
+
 @@include('./_components/_header.js');
 @@include('./_components/_nav.js');
 @@include('./_components/_slider.js');
+@@include('./_components/_swiper.js');
+@@include('./_components/_tabs.js');
 
 
