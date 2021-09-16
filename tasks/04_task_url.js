77749b7a84ef76d9ea90ed6ea4aedcbc415d0cc3
vensky <ivan.fedotovskikh@gmail.com>
2021-09-16 11:02:31 +0500

    Добавляет Задание 4 и 6

--- tasks/04_task_url.js
+++ tasks/04_task_url.js
@@ -1,4 +1,4 @@
-4. Имеются следующие URL
+/*4. Имеются следующие URL
 https://itrack.ru/portfolio/website/
 https://itrack.ru/portfolio/vnedrenie-crm/
 https://itrack.ru/portfolio/website/filter/project_type-is-korporativnyy_sayt/apply/
@@ -14,6 +14,43 @@ https://itrack.ru/portfolio/website/miratorg/
 Параметры фильтра (filterParam) - строка между filter и apply
 Проект (project) - код проекта из url
 
-И при изменении URL без перезагрузки страницы массив должен быть обновлён.
+И при изменении URL без перезагрузки страницы массив должен быть обновлён.*/
+
+const initUrl = () => {
+    let pageParam = [];
+    let path = window.location.pathname;
+    let pageUrl = path.slice(11).split('/');
 
 
+   if (pageUrl[0] === 'website') {
+        pageParam[1] = 'website';
+    } else {
+        pageParam[1] = 'crm';
+    }
+
+    if (pageUrl[1] === '') {
+        pageParam[0] = 'index';
+    } else if (pageUrl[1] === 'filter') {
+        pageParam[0] = 'filter';
+        pageParam[2] = pageUrl[2];
+    } else if (pageUrl[1][0] === '?') {
+        pageParam[0] = 'nextPage';
+    } else {
+        pageParam[0] = 'project';
+        pageParam[1] = pageUrl[1]
+    }
+
+    console.log(pageParam)
+}
+const updateURL = (newUrl) => {
+    if (history.pushState) {
+        history.pushState(null, null, 'https://itrack.ru/portfolio/' + newUrl);
+        initUrl();
+    }
+    else {
+        console.warn('History API не поддерживает ваш браузер');
+    }
+}
+
+updateURL();
+initUrl();
---
+++ tasks/06_task_inherit.js
@@ -0,0 +1,16 @@
+/*6. Приведите пример реализации наследования*/
+class Modal {
+    constructor() {
+
+    }
+
+    method1() {
+
+    }
+}
+
+class ModalLogin extends Modal {
+    method2() {
+
+    }
+}
